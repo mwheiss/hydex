94698538fe2c863ef08926fbb006e65695395436
Michael W. Heiss <michael.heiss@psi.ch>
2026-08-23 21:48:46 +0200

    Add RHEL 7 Hydex package workflow

--- packaging/rpm/build-rhel7-package.sh
+++ packaging/rpm/build-rhel7-package.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-HYDEX_RHEL_MAJOR=10 exec "${script_dir}/build-rpm-package.sh" "$@"
+HYDEX_RHEL_MAJOR=7 exec "${script_dir}/build-rpm-package.sh" "$@"
